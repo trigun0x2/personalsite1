d78e0f41b90d4c783b7e20724764fe9203bbd3e9
Jeffrey Tong <trigun0x2@gmail.com>
2013-09-04 19:51:21 -0600

    forgot to commit...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tingsite::Application.config.secret_key_base = '95cc304c7cf44e5d15f8a6a03fad1904e96a0cfa2e8d2d4222e9984a1945517fb9d3ec4818739c23576f0476e43ecf21e2518b1a065b57383d49614ac4d569ee'
